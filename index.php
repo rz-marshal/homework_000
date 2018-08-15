f64022664d60c656aeda417df833550edfd2bdc2
V. Marshal <lightsourcemarshal@mail.ru>
2018-08-15 20:26:22 +1200

    update to PSR-2

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <?php
-CONST testConstanta = 'asd';
+
+const TEXT_CONSTANT = 'asd';
+
 $user_name = "Igor";
 
-function show_something(){ RETURN 'SOMETHING';}
+function show_something()
+{
+    return 'SOMETHING';
+}
 
-if (1==1) echo 'hi';
+if (1 == 1) {
+    echo 'hi';
+}
